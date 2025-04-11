db5f0a6a1a551aef63f39aa6d12359a6c8f6859f
Aleksandr Mezin <mezin.alexander@gmail.com>
2025-04-11 06:53:51 +0300

    Don't run pytest in check()
    In the end it doesn't seem useful, and only annoys users

--- installed-test/gnome-shell-extension-ddterm-git.sh
+++ installed-test/gnome-shell-extension-ddterm-git.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
-testdir="$(find . -name pytest.ini -printf '%h' -quit)"
+pacman -S --needed --noconfirm python-tox python-gobject wl-clipboard xorg-server-xvfb
 
-exec sudo -E -u builduser pytest -p no:cacheprovider --system-bus "$testdir"
+pytest_args=(
+  -p no:cacheprovider
+  --system-bus
+  --self-contained-html
+  -n auto
+  -vv
+)
+
+exec sudo -E -u builduser tox -c "$(find . -name tox.ini)" --sitepackages -e pytest -- "${pytest_args[@]}"
