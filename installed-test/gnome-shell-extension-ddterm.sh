70744b7f2f6fdfb8039ed0f512fbd11853eb01e8
Aleksandr Mezin <mezin.alexander@gmail.com>
2025-04-29 09:38:31 +0300

    v60

--- installed-test/gnome-shell-extension-ddterm.sh
+++ installed-test/gnome-shell-extension-ddterm.sh
@@ -2,9 +2,4 @@
 
 set -ex
 
-cd gnome-shell-extension-ddterm-59
-curl https://github.com/ddterm/gnome-shell-extension-ddterm/commit/6d61c65e89df2f04b525a27e8085146cb4021694.patch | patch -p1
-
-mkdir -p -m 01777 /tmp/.X11-unix
-
 source "$GITHUB_WORKSPACE/installed-test/gnome-shell-extension-ddterm-git.sh"
