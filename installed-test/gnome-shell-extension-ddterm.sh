6087c9f02aa8959f4ff4ef12486519a5051ec45c
Aleksandr Mezin <mezin.alexander@gmail.com>
2025-06-29 10:23:17 +0300

    Backport test fix

--- installed-test/gnome-shell-extension-ddterm.sh
+++ installed-test/gnome-shell-extension-ddterm.sh
@@ -2,4 +2,7 @@
 
 set -ex
 
+cd gnome-shell-extension-ddterm-61
+curl https://github.com/ddterm/gnome-shell-extension-ddterm/commit/e8d8f6b0d79082ed5e601af1b7beb0917fa44b52.patch | patch -p1
+
 source "$GITHUB_WORKSPACE/installed-test/gnome-shell-extension-ddterm-git.sh"
